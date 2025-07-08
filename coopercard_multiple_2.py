117098998acce25e7296510f2e03002f1b9a4de3
HERICK AKIO YOSHII KUMATA <154487912+haykCAKI@users.noreply.github.com>
2025-07-08 10:56:28 -0300

    colocando a porta para o render

--- coopercard_multiple_2.py
+++ coopercard_multiple_2.py
@@ -169,7 +169,7 @@ def upload_files():
     return render_template_string(HTML_PAGE)
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
 
